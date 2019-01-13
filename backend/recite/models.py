44b3954d77f38c0c34964bcebaaed7220a3ff3d7
Rasul <abuhasub@gmail.com>
2019-01-13 23:26:41 +0100

    Reciting models created
    #9

--- backend/recite/models.py
+++ backend/recite/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db import migrations, models
 
-# Create your models here.
+
+class Reciter(models.Model):
+    '''General information about Reciter'''
+    name = models.CharField(max_length=100)
+    quality = models.CharField(max_length=50, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+
+
+class Recitation(models.Model):
+    '''General information about Recitation'''
+    ayah = models.PositiveIntegerField()
+    segments = models.TextField()
+    surah = models.ForeignKey(
+        'quran.Surah',
+        on_delete=models.CASCADE,
+        related_name='recitation')
+    reciter = models.ForeignKey(
+        Reciter,
+        on_delete=models.CASCADE,
+        related_name='recitation')
