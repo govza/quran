b976dd6ff0519359295a06613061f0a899d78102
Rasul <abuhasub@gmail.com>
2019-01-14 16:04:01 +0100

    Test for quran surahs api
    /api/v1/quran returns list of surahs

--- backend/quran/tests.py
+++ backend/quran/tests.py
@@ -1,4 +1,29 @@
-class TestSimple:
-    def test_one(self):
-        x = "my simple app test"
-        assert 'simple app' in x
+from django.urls import reverse
+from rest_framework.test import APIClient, APITestCase
+from rest_framework.views import status
+
+from .models import Surah
+from .serializers import SurahSerializer
+
+
+class GetQuran(APITestCase):
+    '''Checking Quran app API'''
+    client = APIClient()
+
+    def test_get_surahs(self):
+        """
+        This test ensures that surahs is loaded correctly to database
+        making a GET request to the quran/ endpoint
+        """
+        # hit the API endpoint
+        response = self.client.get(
+            reverse("surahs-all", kwargs={"version": "v1"})
+        )
+        # fetch the data from db
+        expected = Surah.objects.all()
+        serialized = SurahSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # check number of Surahs
+        self.assertEqual(len(response.data), 114)
