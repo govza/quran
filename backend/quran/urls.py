2fcbc50ac0f1ee9536fc8bc0a0b4e13c30852839
Rasul <abuhasub@gmail.com>
2019-01-16 13:39:52 +0100

    Surah details and Ayahs API implemented
    #10

--- backend/quran/urls.py
+++ backend/quran/urls.py
@@ -1,10 +1,17 @@
+'''Quran app api url endpoints'''
 from django.urls import re_path
+
 from . import views
 
 urlpatterns = [
     re_path(
-        'api/(?P<version>(v1|v2))/quran',
+        r'api/(?P<version>(v1|v2))/quran/(?P<surah_id>\d+)/$',
+        views.SurahDetailsView.as_view(),
+        name="surah-details"
+    ),
+    re_path(
+        r'api/(?P<version>(v1|v2))/quran/$',
         views.SurahListView.as_view(),
         name="surahs-all",
-    ),
+    )
 ]
