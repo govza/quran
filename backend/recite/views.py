fe7d29151aa7cee10b9a1a3075dd80c1c66068dd
Rasul <abuhasub@gmail.com>
2019-01-18 15:29:54 +0100

    Views for Reciter List, Reciter Details and Recitation List

--- backend/recite/views.py
+++ backend/recite/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.views import status
 
-# Create your views here.
+from . import serializers
+from .models import Recitation, Reciter
+
+
+class ReciterListView(generics.ListAPIView):
+    '''
+    Provides a get method handler for list of Reciters.
+    '''
+    queryset = Reciter.objects.all()
+    serializer_class = serializers.ReciterSerializer
+
+
+class RecitationListView(generics.ListAPIView):
+    '''
+    Provides a get method handler for list of Surah Recitations.
+    '''
+    serializer_class = serializers.RecitationSerializer
+
+    def get_queryset(self):
+        reciter = self.kwargs['reciter']
+        surah = self.kwargs['surah']
+
+        recitations = Recitation.objects.filter(surah=surah, reciter=reciter)
+
+        if recitations:
+            return recitations
+
+        raise NotFound()
+
+
+class ReciterDetailsView(generics.RetrieveAPIView):
+    '''
+    Provides a get method handler for Reciter information
+    '''
+    queryset = Reciter.objects.all()
+    serializer_class = serializers.ReciterSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            reciter = self.queryset.get(id=kwargs["reciter"])
+            return Response(serializers.ReciterSerializer(reciter).data)
+        except Reciter.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Reciter with id: {} does not exist".format(
+                        kwargs["reciter"])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
