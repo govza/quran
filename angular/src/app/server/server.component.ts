b4e45c90f0f15bbb0f9919a03a3d0cc91ff26b88
Rasul <abuhasub@gmail.com>
2019-01-22 14:32:47 +0100

    ngclass

--- angular/src/app/server/server.component.ts
+++ angular/src/app/server/server.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+      .online {
+        color: white;
+      }
+    `]
 })
 export class ServerComponent implements OnInit {
   serverId: number = 10;
