54919a031add79df51761f37feb9fda7609cc062
Rasul <abuhasub@gmail.com>
2019-01-22 14:27:27 +0100

    Forms bindings

--- angular/src/app/servers/servers.component.ts
+++ angular/src/app/servers/servers.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test Server';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Created server ' + this.serverName;
+    this.serverCreated = true;
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = (event.target).value
+  }
 }
