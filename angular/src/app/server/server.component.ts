0cdd3125b9799a1ea1f51986108448cdd9751d45
Rasul <abuhasub@gmail.com>
2019-01-22 14:27:45 +0100

    Directives

--- angular/src/app/server/server.component.ts
+++ angular/src/app/server/server.component.ts
@@ -3,12 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
-  constructor() { }
+  getServerStatus() {
+    return this.serverStatus
+  }
 
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
   ngOnInit() {
   }
 
